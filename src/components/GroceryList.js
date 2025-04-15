8d0a66585779d2cc2021945fdae9b4ce192a753f
ISWARYA835 <iswarya83p@gmail.com>
2025-04-15 23:12:34 +0530

    css update

--- src/components/GroceryList.js
+++ src/components/GroceryList.js
@@ -8,12 +8,12 @@ const GroceryList = () => {
   if (error) return <p>Error: {error}</p>;
 
   return (
-    <div>
-      <h2>Grocery Products</h2>
+    <div className="border border-black rounded-sm">
+      <h2 className="text-center border border-spacing-4">Grocery Products</h2>
       <ul>
         {grocInfo.map((item) => (
-          <li key={item.id}>
-            <img src={item.image} alt={item.title} width="100" />
+          <li  className='flex flex-wrap' key={item.id}>
+            <img className="text-center" src={item.image} alt={item.title} width="100" />
             <p><strong>{item.title}</strong></p>
             <p>₹{item.price}</p>
             <p>Brand: {item.brand}</p>
