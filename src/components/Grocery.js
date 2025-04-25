7ea44cc16db962e4509a4a13db6caebe313a1cbc
ISWARYA835 <iswarya83p@gmail.com>
2025-04-25 21:55:30 +0530

    css

--- src/components/Grocery.js
+++ src/components/Grocery.js
@@ -1,6 +1,6 @@
 import { useEffect,useState } from "react"; 
 const Grocery =() =>{
-    return <h1>{
+    return <h1 className="border border-black">{
         " " }
         Our Grocery shop has so many varieties of natural and hybrid products which is purelyy chemical free </h1>
 }
