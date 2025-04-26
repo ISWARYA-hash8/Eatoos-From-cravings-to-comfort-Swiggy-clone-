a3196488b1b59284c4ea8046a17e35cc36c4136b
ISWARYA835 <iswarya83p@gmail.com>
2025-04-26 18:17:37 +0530

    changes

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,6 +1,25 @@
+import { CDN_URL } from "../utils/constants";
+
 const ItemList = ({items}) =>{
-    return <div>
-        Category Items
+    console.log(items);
+return (
+    <div>
+        {items.map((item) =>(
+            <div key={item.card.info.id}
+            className="p-2 m-2 border border-black text-left flex ">
+                <div>
+                    <img src={CDN_URL} className="w-20 h-20  border"/>
+                    <div className="py-4 "> 
+                    <span >
+                        {item.card.info.name}
+                    </span>
+                    <span>-Rs.{item.card.info.price /100}</span>
+                </div>
+                {/* <p>{item.card.info.category}</p> */}
+                </div>
+            </div>
+        ))}
     </div>
+    )
 }
 export default ItemList;
--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -2,11 +2,14 @@ import ItemList from "./ItemList";
 
 const RestaurantCategory = ({data}) =>{
     console.log( data);
-    return <div>
-        <div className="w-8/12 mx-auto border border-collapse shadow-slate-500 my-4 bg-gray-50 shadow-lg p-4 flex justify-between" >
-            <span className=" font-serif text-lg font-semibold">{
+    return (
+    <div>
+        <div className="w-8/12 mx-auto border border-collapse shadow-slate-500 my-4 bg-gray-50 shadow-lg p-4 " >
+            <div className="flex justify-between">
+                <span className=" font-serif text-lg font-semibold">{
                data?.title} ({data.itemCards.length})</span>
-            <span>⬇️</span>
+                <span>⬇️</span>
+            </div>
             
         </div>
         <div>
@@ -14,5 +17,6 @@ const RestaurantCategory = ({data}) =>{
         </div>
 
     </div>
+    );
 }
 export default RestaurantCategory;
