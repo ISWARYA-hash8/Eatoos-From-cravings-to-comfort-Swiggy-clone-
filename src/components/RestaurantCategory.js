805b412b6945bb49b0809f9f77aa4ec6b82243bd
ISWARYA835 <iswarya83p@gmail.com>
2025-04-26 15:21:18 +0530

    updated

---
+++ src/components/RestaurantCategory.js
@@ -0,0 +1,18 @@
+import ItemList from "./ItemList";
+
+const RestaurantCategory = ({data}) =>{
+    console.log( data);
+    return <div>
+        <div className="w-8/12 mx-auto border border-collapse shadow-slate-500 my-4 bg-gray-50 shadow-lg p-4 flex justify-between" >
+            <span className=" font-serif text-lg font-semibold">{
+               data?.title} ({data.itemCards.length})</span>
+            <span>⬇️</span>
+            
+        </div>
+        <div>
+            <ItemList  items = {data.itemCards}/>
+        </div>
+
+    </div>
+}
+export default RestaurantCategory;
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -6,6 +6,7 @@ import { useParams } from 'react-router-dom';
 
 import { MENU_API } from "../utils/constants";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
+import RestaurantCategory from "./RestaurantCategory";
 
 //chunking
 // code spilitting 
@@ -22,28 +23,30 @@ const RestaurantMenu = () =>{
  const infor = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.find(
     (c) => c.card?.card?.itemCards
   )?.card?.card;
-  
+  console.log(resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards);
   const itemCards = infor?.itemCards || [];
-  
+  const categories = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((c)=>c.card?.card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
+  console.log(categories);
     return resInfo === null ? (
     <Shimmer />
 ) : ( 
-        <div className="m-4 p-4" >
-            <h1>{name|| "Loading ..."}</h1>
-            <h2>{cuisines}</h2>
+        <div className="text-center" >
+            <h1 className="font-bold my-10 text-2xl">{name|| "Loading ..."}</h1>
+            <h2 className="font-bold text-lg">{cuisines}</h2>
             <h3>{costForTwoMessage}</h3>
             <h2>{avgRating} </h2>
 
-            <h3></h3>
+            
             <h2>Menu</h2>
-            <ul >
+            {/* <ul >
                 {itemCards.map(item =>
                 (<li key ={item.card.info.id}>  
                     {item.card.info.name}  =  {"RS."}{item.card.info.price / 100 || {}}  </li>))
                 }
                 
                 
-            </ul>
+            </ul> */}
+            {categories.map((category) =><RestaurantCategory data={category?.card?.card}/>)}
         </div>
     );
 };
