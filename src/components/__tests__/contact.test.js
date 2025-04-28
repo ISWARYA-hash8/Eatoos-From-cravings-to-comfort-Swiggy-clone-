fe837d586c2c1ae90579422faa31313f29db8e90
ISWARYA835 <iswarya83p@gmail.com>
2025-04-28 19:56:27 +0530

    testing

--- src/components/__tests__/contact.test.js
+++ src/components/__tests__/contact.test.js
@@ -1,1 +1,14 @@
-import {render} from "@"
+import {render,screen} from "@testing-library/react";
+import Contact from "../Contact";
+
+test("Should load the contact us component " ,() =>{
+    render(<Contact/>);
+
+
+    const heading = screen.getByRole("heading");
+       
+    expect(heading).toBeInTheDocument();
+
+
+
+})
