027b16b229d09969ddac39a8a4ef98daf2efa2c9
ISWARYA835 <iswarya83p@gmail.com>
2025-04-28 23:36:52 +0530

    testing

--- src/components/__tests__/contact.test.js
+++ src/components/__tests__/contact.test.js
@@ -1,5 +1,6 @@
 import {render,screen} from "@testing-library/react";
 import Contact from "../Contact";
+import "@testing-library/jest-dom";
 
 test("Should load the contact us component " ,() =>{
     render(<Contact/>);
@@ -7,8 +8,34 @@ test("Should load the contact us component " ,() =>{
 
     const heading = screen.getByRole("heading");
        
-    expect(heading).toBeInTheDocument();
+    expect(heading).toBeInTheDocument(); 
 
 
 
-})
+});
+
+// test("Should load the button inside contact  component " ,() =>{
+//     render(<Contact/>);
+
+
+//     const button = screen.getByText("Submit");
+       
+//     expect(button).toBeInTheDocument(); 
+
+
+
+// });
+
+// test("Should load the button inside contact  component " ,() =>{
+//     render(<Contact/>);
+
+
+//     const Iname = screen.getByRole("name");
+       
+//     expect(Iname).toBeInTheDocument(); 
+
+
+
+// });
+
+
